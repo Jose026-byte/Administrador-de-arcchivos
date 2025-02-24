cbae3b70ada436f3a0f6f3d7378dff7ae58f1059
Jose026-byte <al07029721@tecmilenio.mx>
2025-02-24 10:29:19 -0600

    Update ListaArchivos.java

--- src/me/jmll/ListaArchivos.java
+++ src/me/jmll/ListaArchivos.java
@@ -6,9 +6,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
